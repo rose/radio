20b127c1e7c02874bf0ed05e4d6f1a66e6567418
Rose Ames <rose@happyspork.com>
2014-06-07 15:45:22 -0400

    pass title info to edit templates

--- logger/views.py
+++ logger/views.py
@@ -6,6 +6,7 @@ class ListShowView(ListView):
     model = Show
     template_name = "show_list.html"
 
+
 class EditShowView(ListView):
     template_name = "edit_show.html"
 
@@ -13,9 +14,20 @@ class EditShowView(ListView):
         self.show = get_object_or_404(Show, id=self.kwargs['pk'])
         return Episode.objects.filter(show=self.show)
 
+    def get_context_data(self, **kwargs):
+        ctx = super(EditShowView, self).get_context_data(**kwargs)
+        ctx['show'] = self.show
+        return ctx
+
+
 class EditEpisodeView(ListView):
     template_name = "edit_episode.html"
 
     def get_queryset(self):
         self.ep = get_object_or_404(Episode, id=self.kwargs['pk'])
         return Segment.objects.filter(episode=self.ep)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(EditEpisodeView, self).get_context_data(**kwargs)
+        ctx['episode'] = self.ep
+        return ctx
