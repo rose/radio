46c5073caf559338dd4d01f6a79ad2e873956056
Rose Ames <rose@happyspork.com>
2014-06-08 13:46:37 -0400

    added bootstrap to edit_* templates

--- radio/urls.py
+++ radio/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,4 +9,4 @@ urlpatterns = patterns('',
     #url(r'^$', 'radio.views.home', name='home'),
     url(r'^logger/', include('logger.urls'), name='logger'),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
