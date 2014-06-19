1e2c07697317dfdb9dc60ec4a49304f33f095182
Rose Ames <rose@happyspork.com>
2014-06-19 08:16:53 -0400

    add form to edit episode view

--- logger/views.py
+++ logger/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, View
+from django.forms import ModelForm
 from logger.models import Show, Episode, Segment, Song
+
 
 class ListShowView(ListView):
     model = Show
@@ -20,7 +22,7 @@ class EditShowView(ListView):
         return ctx
 
 
-class EditEpisodeView(ListView):
+class EpisodeSegmentsView(ListView):
     template_name = "edit_episode.html"
 
     def get_queryset(self):
@@ -28,6 +30,27 @@ class EditEpisodeView(ListView):
         return Segment.objects.filter(episode=self.ep)
 
     def get_context_data(self, **kwargs):
-        ctx = super(EditEpisodeView, self).get_context_data(**kwargs)
+        ctx = super(EpisodeSegmentsView, self).get_context_data(**kwargs)
         ctx['episode'] = self.ep
+        ctx['form'] = SegmentForm()
         return ctx
+
+
+class SegmentForm(ModelForm):
+    class Meta:
+        model = Segment
+
+
+class AddSegmentView(CreateView):
+    model = Segment
+    form_class = SegmentForm
+
+
+class EditEpisodeView(View):
+    def get(self, request, *args, **kwargs):
+        view = EpisodeSegmentsView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = AddSegmentView.as_view()
+        return view(request, *args, **kwargs)
