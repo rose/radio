0c3645ee459403574454744dbb7bb694a81c0286
Daryl Anderman <daryl@273kelvin.ca>
2014-08-25 12:19:25 -0400

    add basic test framework with selenium

--- logger/tests.py
+++ logger/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+class LogAShow(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_login(self):
+        # TODO add this when we add users
+        #browser.get('http://localhost:8000')
+
+        self.browser.get('http://localhost:8000/logger')
+        self.assertIn('CHCR CRTC', self.browser.title)
+        self.fail('Need to add users!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
