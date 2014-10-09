1e72afc792e4204f0ca0ad4ba8b60204c95ff124
Daryl Anderman <daryl@273kelvin.ca>
2014-10-09 08:20:23 -0400

    add login url

--- logger/urls.py
+++ logger/urls.py
@@ -3,6 +3,8 @@ from logger.views import *
 
 urlpatterns = [
     url(r'^$', ListShowView.as_view(), name='shows'),
+    url(r'^login$', 'django.contrib.auth.views.login', 
+        {'template_name': 'login.html'}, name='login'),
     url(r'^show/(?P<pk>\d+)$', EditShowView.as_view(), name='edit-show'),
     url(r'^episode/(?P<pk>\d+)$', EditEpisodeView.as_view(), name='edit-episode'),
 ]
